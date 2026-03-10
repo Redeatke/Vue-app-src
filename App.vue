c12703ae981465a5e81364e54ce235d40524860c
Red Keb <rkebede@eagles.bridgewater.edu>
2026-03-10 02:41:52 -0400

    app6 src code

---
+++ App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import FooterView from '@/components/FooterView.vue'
+</script>
+
+<template>
+  <div class="app">
+    <NavBar />
+    <RouterView class="view" />
+    <FooterView />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  height: 100vh;
+  width: 100vw;
+  box-sizing: border-box;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+.view {
+  position: relative;
+}
+.view::after {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  content: '';
+  background-image: url(/starfish.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  filter: grayscale(100%);
+  opacity: 0.05;
+  z-index: -1;
+}
+</style>
+
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
---
+++ views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed } from 'vue'
+import { useUserStore } from '@/stores/user'
+import { useRouter } from 'vue-router'
+
+const store = useUserStore()
+const router = useRouter()
+
+const welcomeName = computed(() => {
+  if (store.user.username) {
+    const name = store.user.username
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  } else {
+    router.push('/login')
+  }
+})
+</script>
+
+<template>
+  <div class="home-view">
+    <h1>Welcome {{ welcomeName }}!</h1>
+  </div>
+</template>
+
+<style scoped>
+.home-view {
+  box-sizing: border-box;
+  padding: 20px;
+  background-color: #efeded;
+  opacity: 97%;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+</style>
+
