9f09d5d6ba643134282ade6558e1bc60981365ce
Red Keb <rkebede@eagles.bridgewater.edu>
2026-04-21 01:33:14 -0400

    Clean up stray files below app10

--- stores/user.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-import { reactive } from 'vue'
-
-export const useUserStore = defineStore('userStore', () => ({
-  user: reactive({
-    username: localStorage.getItem('username')
-  })
-}))
